49dc868b494d5f52075465e68a88b32ae9799b01
Mércia Maguerroski <merciasmc@outlook.com>
2019-08-23 14:15:10 -0300

    start the development

---
+++ TodoList/TodoList/ChecklistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Mercia Maguerroski on 23/08/19.
+//  Copyright © 2019 Mércia. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
