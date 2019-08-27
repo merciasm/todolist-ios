ab9e0f9940ebae7877eee9e8c56402605e80c433
Mércia Maguerroski <merciasmc@outlook.com>
2019-08-27 12:47:52 -0300

    make the space to add more items

--- TodoList/TodoList/AddItemTableViewController.swift
+++ TodoList/TodoList/AddItemTableViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func cancel(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+        print("Content of the text field \(textField.text)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
     }
 
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
 
 }
